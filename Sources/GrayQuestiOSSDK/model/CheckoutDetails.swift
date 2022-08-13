51c37c101feccb512e191d6afea91435dd16baa3
cool12 <msatao75@gmail.com>
2022-08-13 12:35:32 +0530

    debug 3

--- Sources/GrayQuestiOSSDK/model/CheckoutDetails.swift
+++ Sources/GrayQuestiOSSDK/model/CheckoutDetails.swift
@@ -9,7 +9,14 @@ import Foundation
 
 public struct CheckoutDetails {
     
-    public init(order_id: String, razorpay_key: String, recurring: Bool, notes: [String : Any], customer_id: String, callback_url: String) { }
+    public init(order_id: String, razorpay_key: String, recurring: Bool, notes: [String : Any], customer_id: String, callback_url: String) {
+        self.order_id = order_id
+        self.razorpay_key = razorpay_key
+        self.recurring = recurring
+        self.notes = notes
+        self.customer_id = customer_id
+        self.callback_url = callback_url
+    }
     
     public var order_id: String?
     public var razorpay_key: String?
